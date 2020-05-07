9712ef7a86d57b86895e943c18edea7c7530cedc
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-07 11:10:58 -0400

    refactored knexconfig object to illustrate an example of the live connection to the database

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const knex = require('knex');
 
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/produce.db3'
-  },
-  useNullAsDefault: true
-});
-
 const router = express.Router();
+
+const knexConfig = {
+  client: "sqlite3", //database driver
+  connection: { //this connection property can be an object or a string
+    filename: './data/produce.db3' // This pathway needs to be written from the perspective of the root of the project file
+  },
+  useNullAsDefault: true, //This is something only required for SQLite
+};
+
+// this db variable represents a live connection to the database
+const db = knex(knexConfig);
+
 
 router.get('/', (req, res) => {
   db('fruits')

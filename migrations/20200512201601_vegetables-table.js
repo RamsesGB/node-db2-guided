5b8401f167056c467a08a5b5931a06e6bce0b5ac
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-12 21:42:44 -0400

    ran vegetable table migration

--- migrations/20200512201601_vegetables-table.js
+++ migrations/20200512201601_vegetables-table.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+// "UP" describes the changes that will be applied to the database
+exports.up = function (knex) {
+  //create the vegetables table
+  return knex.schema.createTable('vegetables', (tbl) => {
+    // <---- remember to 'return' the call to knex.schema
+    // 1st thing you want to do is add a named ID that is your primary key that is type interger that autoincrements
+    tbl.increments();
+    tbl.string('name', 255).notNullable().unique().index();
+    tbl.boolean('favorite');
+  });
 };
 
-exports.down = function(knex) {
-  
+// "DOWN" describes how to undo the changes from the up function
+exports.down = function (knex) {
+  //remove (drop) the vegetables table
+  return knex.schema.dropTableIfExists('vegetables');
 };

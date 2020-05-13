8b88e9d23480b77ba5b159e77a2f74bf6eed321a
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-12 22:08:49 -0400

    filled in code in new add color migration

--- migrations/20200512220552_add-vegetable-color.js
+++ migrations/20200512220552_add-vegetable-color.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.table('vegetables', (tbl) => {
+    tbl.string('color', 80);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.table('vegetables', (tbl) => {
+    tbl.dropColumn('color');
+  });
 };

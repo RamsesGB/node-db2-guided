847c756cf34cafb214fb61552387576c32b959f8
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-12 21:44:23 -0400

    changed database to market in the knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'sqlite3', //database driver
     connection: {
       //this connection property can be an object or a string
-      filename: './data/produce.db3', // This pathway needs to be written from the perspective of the root of the project file
+      filename: './data/market.db3', // This pathway needs to be written from the perspective of the root of the project file
     },
     useNullAsDefault: true, //This is something only required for SQLite
   },
